471b4467b5a04e240bcca45b73e248735309e88c
Jake Mussler <jake.mussler@protonmail.com>
2022-03-24 14:38:07 +0000

    changed delay to use asm nop instruction

--- src/main.c
+++ src/main.c
@@ -10,10 +10,10 @@
 #define USART0						((USART_TypeDef *) USART0_BASE)
 
 // Loop <delay> times in a way that the compiler won't optimize away
-static inline void delay(int32_t count)
-{
-	asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
-		 : "=r"(count): [count]"0"(count) : "cc");
+static inline void delay(int32_t count) {
+	for (int32_t i = 0; i < count; i++) {
+		asm volatile("nop");
+	}
 }
 
 /* Write to a pointer with raw value, ensure mask >= value 
