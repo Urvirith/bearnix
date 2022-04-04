a9fa4966ba76d06af0d42f6b2eb25679fc20cba9
Jake Mussler <jake.mussler@protonmail.com>
2022-04-04 17:29:55 +0100

    begin looking at tasks

--- bcm2837_hal/inc/main.h
+++ bcm2837_hal/inc/main.h
@@ -1,6 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 #include "common.h"
 #include "pointer.h"
 #include "bcm2837.h"
 #include "gpio.h"
 #include "mailbox.h"
 #include "usart.h"
+
+
+typedef struct sys_task {
+    u64 id;         // Task ID
+    u64 addr;       // Task Address
+    u64 spos;       // Task Previous Stack Position
+    u64 tcbloc;     // TCB Location
+} SysTask_TypeDef;
+
+#endif
